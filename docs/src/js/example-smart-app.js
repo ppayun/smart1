70b8cd406796df5ce48f28313795341e1a8e7ba5
Steven Wu <Swu@imo-online.com>
2017-07-19 15:21:19 -0500

    test

--- docs/src/js/example-smart-app.js
+++ docs/src/js/example-smart-app.js
@@ -1,13 +1,14 @@
 (function(window){
   window.extractData = function() {
     var ret = $.Deferred();
-
+	
     function onError() {
       console.log('Loading error', arguments);
       ret.reject();
     }
 
     function onReady(smart)  {
+	  window.smart = smart;
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
